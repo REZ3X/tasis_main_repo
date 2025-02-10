603b67f3e0179a08953297299eee1c0e9b74c99f
REZ3X <rezex.dev01@gmail.com>
2025-02-10 22:10:42 +0700

    Add custom font family 'inter-tight' to Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        'inter-tight': ['var(--font-inter-tight)', 'sans-serif'],
+      },
     },
   },
   plugins: [],
