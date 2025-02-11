a429ba26b9c1679e7dd2e97854ce17388910644f
REZ3X <rezex.dev01@gmail.com>
2025-02-11 21:00:57 +0700

    Add MobileNav component to enhance navigation on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import Skills from '@/components/services/skills';
 import MailForm from '@/components/contact/mailForm';
 import Footer from '@/components/footer/footer';
 import ButtonToUp from '@/components/feature/buttonToUp';
+import MobileNav from '@/components/nav/mobileNav';
 
 export default function Home() {
   return (
     <>
       <DesktopNav />
+      <MobileNav />
       <main className="min-h-screen bg-[#0d1216] pt-20"> {/* Added pt-20 for navbar spacing */}
         <Hero />
         <MissionVision />
