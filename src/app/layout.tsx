680672e8bfd49974d8e777844443b277737f32df
REZ3X <rezex.dev01@gmail.com>
2025-02-12 21:28:49 +0700

    Remove metadata export from layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "TASIS",
-  description: "Tata Tertib Siswa",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
