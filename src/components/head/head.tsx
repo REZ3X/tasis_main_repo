69f11d31de70dfbdf0fbda523c0fc475164d38f3
REZ3X <rezex.dev01@gmail.com>
2025-02-12 21:26:33 +0700

    Refactor head component: standardize font import syntax and update meta tags

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -1,10 +1,10 @@
-import { Inter_Tight } from 'next/font/google';
+import { Inter_Tight } from "next/font/google";
 
 const interTight = Inter_Tight({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter-tight',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], // Changed from weights to weight
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter-tight",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Changed from weights to weight
 });
 
 export { interTight };
@@ -18,10 +18,13 @@ export default function Head() {
         href="https://fonts.gstatic.com"
         crossOrigin="anonymous"
       />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="TASIS - Tim Tata Tertib Siswa Stembayo" />
-      <title>TASIS | Tata Tertib Siswa</title>
       <link rel="icon" href="/images/logo.svg" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="description"
+        content="TASIS - Tim Tata Tertib Siswa Stembayo"
+      />
+      <title>TASIS | Tata Tertib Siswa</title>
     </>
   );
 }
