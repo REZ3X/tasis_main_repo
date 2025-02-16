fe1433eec13659b57742d7a4ff7436003ba405ae
REZ3X <rezex.dev01@gmail.com>
2025-02-16 11:50:46 +0700

    Update footer email address to new domain

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -106,11 +106,11 @@ export default function Footer() {
               </li>
               <li>
                 <a
-                  href="mailto:tasis.stembayo.sch.id"
+                  href="mailto:tasis@stembayo.sch.id"
                   className="flex items-center gap-3 text-gray-300 hover:text-[#ebae3a] transition-colors duration-300 group"
                 >
                   <FaEnvelope className="text-[#ebae3a] group-hover:scale-110 transition-transform duration-300" />
-                  <span>tasis.stembayo.sch.id</span>
+                  <span>tasis@stembayo.sch.id</span>
                 </a>
               </li>
               <li>
